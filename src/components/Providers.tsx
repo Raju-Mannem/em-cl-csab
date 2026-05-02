b04ce199f768765624ae524f4e9337c69b02858c
Raju Mannem <rajumannem71@gmail.com>
2026-05-02 15:56:54 +0530

    fix: apollo client optimization

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,12 +1,16 @@
-"use client"
-import React from "react"
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
-import { ApolloProvider } from "@apollo/client/react"
+"use client";
 
-export const Providers=({children} : {children: React.ReactNode}) =>{
-    const client= new ApolloClient({
-        link: new HttpLink({uri : process.env.NEXT_PUBLIC_GRAPHQL_API_URL}),
-        cache: new InMemoryCache(),
-    })
-    return <ApolloProvider client={client}>{children}</ApolloProvider>;
+import React from "react";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_API_URL || "/api/graphql",
+  }),
+  cache: new InMemoryCache(),
+});
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
